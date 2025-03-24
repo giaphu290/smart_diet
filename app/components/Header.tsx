703ad11fc321dabbc670b5ffb756300ff7c3763b
s3rcc <s3rcc.9@gmail.com>
2025-03-24 17:08:56 +0700

    update file

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
             </motion.a>
             
             <motion.a
-              href="/smartdiet.apk"
+              href="../apk/smart-diet.apk"
               download
               className="flex items-center space-x-2 bg-gradient-to-r from-green-600 to-teal-600 text-white px-6 py-2 rounded-full shadow-lg hover:shadow-green-500/25"
               whileHover={{ scale: 1.05 }}
